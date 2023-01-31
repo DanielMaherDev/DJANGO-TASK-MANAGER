44d3ec9953683c4243016d98663510879a2e73f8
DanielMaherDev <dmdanielmaher@gmail.com>
2023-01-31 00:09:42 +0000

    setup remaining tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,2 +1,7 @@
 from django.test import TestCase
-from .forms import ItemForm
+from .models import Item
+
+class TestModel(TestCase):
+    def test_default_done_status(self):
+        item = Item.objects.create(name='test todo item')
+        self.assertFalse(item.done)
